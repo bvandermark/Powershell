5388143f7f6e4777b0a98726c9bbd219f60b439f
Brandon VanDermark <brandon.vandermark@rackspace.com>
2015-09-02 14:41:55 -0500

    Contains improvements to log parsing to minimize RAM consumption and extends functionality to consume multiple logs or IIS Site logs.

--- LogReader.ps1
+++ LogReader.ps1
@@ -1,13 +1,44 @@
-function LogReader{
+function Search-LogFiles{
 	param(
-		[Parameter(Mandatory=$True,Position=1,ValueFromPipeline=$true)]
-		$path=$_
+		[Parameter(Position=1,ValueFromPipeline=$true)]
+		[String[]]$path=$_,
+        [String[]]$website=$null,
+        [ValidateSet("W3SVC")]
+        $logFormat="W3SVC"
 	)
+    
+    foreach($site in $website){
+        if((Get-Website $site) -ne $null){
+            $siteInfo = Get-Website $site
+            $siteId = $siteInfo.Id
+            $logDir = ($siteInfo.LogFile.directory) + "\W3SVC" + $siteId
+            if($logDir.Contains("%SystemDrive%")){
+                $logDir = $logDir.Replace("%SystemDrive%", "$env:SystemDrive")
+            }
+            if(Test-Path $logDir){
+                $path = gci $logDir | select -exp FullName
+            }
+        }
+    }
 	
-	if(Test-Path $path){
-		$rawLog = [System.IO.File]::ReadAllLines($path)
-		$headers = $rawLog[3].split(" ") | ?{$_ -ne "#Fields:"}
-		$logData = Import-Csv -Delimiter " " -Header $headers -Path $path | ?{$_.date -notlike "#*"}
-		$logData
-	}
-}
+	foreach($logFile in $path){
+        if(Test-Path $logFile){
+		    switch($logFormat){
+                W3SVC{
+                    $headers = ([System.IO.File]::ReadLines($logFile) | select -Index 3).split(" ") | ?{$_ -ne "#Fields:"}
+                    foreach($line in [System.IO.File]::ReadLines($logFile)){
+                        if($line.StartsWith("#Fields:")){
+                            $headers = $line.split(" ") | ?{$_ -ne "#Fields:"}
+                        }
+                        elseif(-not $line.StartsWith("#")){
+                            ConvertFrom-Csv -Delimiter " " -Header $headers -InputObject $line
+                        }
+                    }
+                }
+            }
+	    }
+        else{
+            Write-Host "No file found at $($logFile). Please check file location and try again."
+        }
+    }
+} 
